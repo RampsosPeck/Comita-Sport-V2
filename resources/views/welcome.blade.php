4128fe6856e55bdaf14abf872c6473c206d12b6a
RampsosPeck <peralta.jorge.uatf@gmail.com>
2020-05-28 02:30:29 -0400

    Finish fron end styles complet perfect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,19 @@
 
 @section('saludo')
 <div class="caratula-ind col-md-6">
-    <h1 style="color: #fff;"> <b> "Sport La Comita" </b></h1>
-    <h2 class="lead text-justify" style="color: #ffd700;"><strong> Bienvenidos a nuestra página web, registrate para poder realizar pedidos y cotizaciones en linea y te contactaremos para coordinar la entrega, puedes realizar tus pedidos cuando estés seguro.</strong>
-    </h2>
+    <h1 class="text-white"> <b> "Sport La Comita" </b></h1>
+    <span class="lead text-justify text-warning d-none d-sm-none d-md-block"  ><strong> Bienvenidos a nuestra página web, registrate para poder realizar pedidos y cotizaciones en linea y te contactaremos para coordinar la entrega, puedes realizar tus pedidos cuando estés seguro.</strong>
+    </span>
 
     @if (Route::has('login'))
             {{-- "auth" verifica si se authentico el usuario--}}
             @auth
                 <a href="{{ url('/admin') }}"  class="btn btn-lg  btn-outline-light" >INICIO</a>
             @else
-                <a href="{{ route('login') }}" class="btn btn-lg  btn-light">INGRESAR</a>
+                <a href="{{ route('login') }}" class="btn btn-block   btn-light">INGRESAR</a>
 
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="btn btn-lg  btn-outline-light">REGISTRARSE</a>
+                    <a href="{{ route('register') }}" class="btn btn-block  btn-outline-light">REGISTRARSE</a>
                 @endif
             @endauth
     @endif
